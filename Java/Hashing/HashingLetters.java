3f62406a696e2cec29cafe63b62a8d53003b9ef1
mohammedabdulbari <mohammed.bari@gmail.com>
2024-09-13 15:52:20 +0530

    Create HashingLetters.java

---
+++ Java/Hashing/HashingLetters.java
@@ -0,0 +1,25 @@
+class HashingLetters
+{
+    static char mostOccuringLetter(String str)
+    {
+        str=str.toLowerCase();
+        char letters[]=str.toCharArray();
+
+        int hash[]=new int[26];
+        for(int i=0;i<letters.length;i++)
+        {
+            hash[letters[i]-97]++;
+        }
+        int max=0;
+        for(int i=0;i<hash.length;i++)
+            if(hash[i]>hash[max]) max=i;
+        
+        return (char)(max+97);
+    }
+    public static void main(String args[])
+    {
+        String str="mkbqsqjbyq";
+        char result=mostOccuringLetter(str);
+        System.out.println(result);
+    }
+}
