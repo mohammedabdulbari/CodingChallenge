1ea6ff4735768359f311a6014feb2682d464da09
mohammedabdulbari <mohammed.bari@gmail.com>
2024-09-14 00:34:49 +0530

    Create ThirdMaximum.java

---
+++ Java/SinglePointer/ThirdMaximum.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+class ThirdMaximum
+{
+    static int thirdMaximum(int nums[])
+    {
+        int firstMax=Integer.MIN_VALUE;
+        int secondMax=Integer.MIN_VALUE;
+        int thirdMax=Integer.MIN_VALUE;
+        for(int i=0;i<nums.length;i++)
+        {
+            if(nums[i]>firstMax)
+            {
+                thirdMax=secondMax;
+                secondMax=firstMax;
+                firstMax=nums[i];
+            }
+            else if(nums[i]>secondMax)
+            {
+                thirdMax=secondMax;
+                secondMax=nums[i];
+            }
+            else if(nums[i]>thirdMax)
+                thirdMax=nums[i];
+        }
+        return thirdMax;
+    }
+    public static void main(String args[])
+    {
+        int nums[]={8,5,3,11,9,4,16,7,12,13};
+        System.out.print(thirdMaximum(nums));
+    }
+}
