a8ab76b49ed4f90b11f90fa752cceeab0747bf72
mohammedabdulbari <mohammed.bari@gmail.com>
2024-09-13 17:26:05 +0530

    Create SlidingWindow.java

---
+++ Java/SlidingWindow/SlidingWindow.java
@@ -0,0 +1,26 @@
+class SlidingWindow
+{
+    static int maxSubArray(int nums[],int k)
+    {
+        int i=0,j=0;
+        int sum=0;
+        for(;j<k;j++)
+            sum+=nums[j];
+        int max=sum;
+        while(j<nums.length-1)
+        {
+            sum-=nums[i++];
+            sum+=nums[j++];
+            if(sum>max)
+                max=sum;
+        }
+        
+        return max;
+    }
+    public static void main(String args[])
+    {
+        int nums[]={1,2,0,4,3,6,2,1,9,-1};
+        int result=maxSubArray(nums,3);
+        System.out.println(result);
+    }
+}
