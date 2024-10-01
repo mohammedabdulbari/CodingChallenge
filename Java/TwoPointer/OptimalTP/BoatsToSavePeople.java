bf6fd1c2b3f196dcc076b5d7ce7b0488dbb79be2
mohammedabdulbari <mohammed.bari@gmail.com>
2024-10-01 21:13:52 +0530

    Create BoatsToSavePeople.java

---
+++ Java/TwoPointer/OptimalTP/BoatsToSavePeople.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+class BoatsToSavePeople
+{
+    public static int numRescueBoats(int[] people, int limit) {
+        Arrays.sort(people);
+        int i=0,j=people.length-1,count=0;
+        while(i<=j)
+        {
+            if(i!=j && people[i]+people[j]<=limit)
+            {
+                count++;
+                i++;j--;
+            }
+            else
+            {
+                count++;
+                j--;
+            }
+        }
+        return count;
+        
+    }
+       public static void main(String[] args)
+    {
+        int[] arr = {4,2,8,3,1,6,2,5};
+        int res=numRescueBoats(arr,8);
+        System.out.print(res);
+    }
+}
