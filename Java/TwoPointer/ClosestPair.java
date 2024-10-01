247b36d833b92a2dc8a82b3fc3ef05171c3ac162
mohammedabdulbari <mohammed.bari@gmail.com>
2024-10-01 20:25:52 +0530

    Create ClosestPair.java

---
+++ Java/TwoPointer/ClosestPair.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+class ClosestPair
+{
+    public static List<Integer> findClosestElements(int[] arr, int k, int x) {
+
+        int l = 0;
+        int r = arr.length - 1;
+        while( r-l+1 > k)
+        {
+           if(Math.abs(arr[l] - x) > Math.abs(arr[r] - x)){
+               l++;
+
+           }
+           else
+           {
+               r--;
+           }
+        }
+        List<Integer> listof = new ArrayList<>();
+        for(int i = l; i <= r; i++)
+        {
+            listof.add(arr[i]);
+        }
+        return listof;
+        
+    }
+    public static void main(String[] args)
+    {
+        int[] arr = {1,3,5,7,9,11,13,15};
+        List<Integer> al=findClosestElements(arr,3,7);
+        for(int x:al)
+        System.out.print(x+ " ");
+    }
+}
