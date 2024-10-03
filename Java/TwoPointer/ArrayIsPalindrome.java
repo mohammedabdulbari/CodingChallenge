243cc19fdf341e8db64c4ff1aeb54ec6da8040f6
mohammedabdulbari <mohammed.bari@gmail.com>
2024-10-03 16:22:19 +0530

    Create ArrayIsPalindrome.java

---
+++ Java/TwoPointer/ArrayIsPalindrome.java
@@ -0,0 +1,20 @@
+public class ArrayIsPalindrome{
+    static boolean isPalindrome(int arr[])
+    {
+        int i=0;
+        int j=arr.length-1;
+        while(i<j)
+        {
+            if(arr[i]!=arr[j])
+              return false;
+            i++;
+            j--;
+        }
+        return true
+    }
+    public static void main(String args[])
+    {
+        int arr[]={1,2,3,4,3,2,1};
+        System.out.println(isPalindrome(arr);
+    }
+}
