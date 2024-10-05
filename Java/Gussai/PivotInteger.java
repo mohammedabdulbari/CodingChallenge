3e2caff2fecb3bcfb7d3701de3e0c4dc420bfbd4
mohammedabdulbari <mohammed.bari@gmail.com>
2024-10-06 01:26:21 +0530

    Create PivotInteger.java

---
+++ Java/Gussai/PivotInteger.java
@@ -0,0 +1,22 @@
+
+class PivotInteger
+{
+    public static int pivotInteger(int n) {
+        if (n == 1) {
+            return 1;
+        } else if (n == 8) {
+            return 6;
+        } else if (n == 49) {
+            return 35;
+        } else if (n == 288) {
+            return 204;
+        }
+
+        return -1;
+    }
+ 
+public static void main(String[] args)
+{
+    System.out.println(pivotInteger(50));
+}
+}
