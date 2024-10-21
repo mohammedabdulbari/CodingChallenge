88282e50a5f6e8125f53328829d3085937b820f0
mohammedabdulbari <mohammed.bari@gmail.com>
2024-10-22 00:44:29 +0530

    Create FindKthMissing.java

---
+++ Java/BinarySearch/FindKthMissing.java
@@ -0,0 +1,27 @@
+
+class FindKthMissing
+{
+    static int findKthPositive(int[] arr, int k) 
+    {
+        int low = 0, high = arr.length - 1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            int totMissing = arr[mid] - (mid + 1);
+
+            if (totMissing < k)
+                low = mid + 1;
+            else
+                high = mid - 1;
+        }
+
+        return k + high + 1;
+    }
+public static void main(String[] args)
+{
+    int nums[]={2,3,4,7,11};
+    int res=findKthPositive(nums,1);
+    System.out.println(res);
+}
+}
