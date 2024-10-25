afb7057a5a9584a219a8e6ac0fdf2d97ce29c584
mohammedabdulbari <mohammed.bari@gmail.com>
2024-10-26 00:52:56 +0530

    Update PivotInteger.java

--- Java/BinarySearch/PivotInteger.java
+++ Java/BinarySearch/PivotInteger.java
@@ -1,6 +1,6 @@
 class PivotInteger
 {
-    public static int pivotInteger(int n) {
+   /* public static int pivotInteger(int n) {
         if (n == 1) {
             return 1;
         } else if (n == 8) {
@@ -9,6 +9,23 @@ class PivotInteger
             return 35;
         } else if (n == 288) {
             return 204;
+        }
+
+        return -1;
+    }*/
+
+    int pivotInteger(int n) {
+        int total=n*(n+1)/2;
+        int l=1,h=n;
+        while(l<=h)
+        {
+            int m=(l+h)/2;
+            if(m*m == total)
+                return m;
+            if(m*m < total)
+                l=m+1;
+            else
+                h=m-1;
         }
 
         return -1;
